c655ccd91f4b823f38abc9a2959162044ccea937
jaymuk <jaymuk.business@gmail.com>
2015-10-17 16:19:18 +0100

    added bower for frontend packages, styling for navbar including modals

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :views
   devise_for :users
   resources :listings
   # The priority is based upon order of creation: first created -> highest priority.
