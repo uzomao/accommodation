430c3fb9fca4ec0649b04dadf066a47fd74cfabb
Chidumaga <chidumaga@gmail.com>
2015-11-20 17:25:15 +0000

    closes #52

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -92,7 +92,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
  
   # Modified Devise params for user login
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :image)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :image, :is_landlord)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115023801) do
+ActiveRecord::Schema.define(version: 20151120171207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151115023801) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.boolean  "is_landlord"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
