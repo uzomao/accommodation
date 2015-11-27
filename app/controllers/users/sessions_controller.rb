407cd2ddeb857c77b3100229a3d1e457ee543c33
Chidumaga <chidumaga@gmail.com>
2015-11-27 00:21:28 +0000

    updates 68

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,12 +4,11 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_to do |format|
       format.js {
-            flash[:notice] = "Created account, signed in."
+            set_flash_message(:notice, :signed_in)
             render :template => "remote_content/devise_success_sign_up.js.erb"
             flash.discard
           }
