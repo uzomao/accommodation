46f6e48c51601d67dc2453b728043c14fb257f85
jaymuk <jaymuk.business@gmail.com>
2016-01-19 18:28:55 +0000

    database seeding functioning somewhat + carousel working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# clean listings from database
+Listing.delete_all
+User.delete_all
+Picture.delete_all
+
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+end
+
+
+
+
+user = User.create(email: "jack@mail.com", first_name: "Jack", last_name: "Jansang", encrypted_password: "1234567890")
+listing = Listing.create!(name: "Hotel Lunde", address: "123 Douala Road", city: "Douala", price: 3000, user_id: user.id)
+image1 = Picture.create({
+  # :id => 52, 
+  # asset: File.new("#{Rails.root}/path/to/somefile.jpg"),
+  image: File.new(File.join(Rails.root, "/app/assets/images/seed/frog.jpg")),
+  listing_id: listing.id
+})
