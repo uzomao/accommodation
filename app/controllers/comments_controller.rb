87ade2ab5e9c1003335fb4753002f9f7a900d51d
Chidumaga <chidumaga@gmail.com>
2016-01-21 20:03:08 +0000

    changes 54

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,22 +3,30 @@ class CommentsController < ApplicationController
 	def new
       @listing = Listing.find(params[:listing_id])
   		@comment = Comment.new(parent_id: params[:parent_id])
+      p params[:parent_id]
 	end
 
 	def create
-      if params[:comment][:parent_id].to_i > 0
-        parent = Comment.find_by_id(params[:comment].delete(:parent_id))
-        @comment = parent.children.build(comment_params)
-      else
+    if params[:comment][:parent_id].to_i > 0
+      parent = Comment.find_by_id(params[:comment].delete(:parent_id))
+      @comment = parent.children.build(comment_params)
+      success
+    else
   		@listing = Listing.find(params[:listing_id])
   		@listing.comments.create(comment_params)
   		Comment.last.update_columns(user_id: current_user.id)
-      flash[:success] = 'Your comment was successfully added!'
-  		redirect_to listing_path(@listing.id)
-	end
+      success
+    end
+  end
+
 
 	def comment_params
-  		params.require(:comment).permit(:thoughts)
+  	params.require(:comment).permit(:thoughts)
 	end
+
+  def success
+    flash[:success] = 'Your comment was successfully added!'
+    redirect_to listing_path(@listing.id)
+  end
 
 end
