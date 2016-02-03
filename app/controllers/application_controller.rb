6dec451369b29ec283aad0dc50aa45b0d919fa37
jaymuk <jaymuk.business@gmail.com>
2016-02-03 19:40:23 +0000

    fixed CSRF error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   respond_to :html, :json
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 end
