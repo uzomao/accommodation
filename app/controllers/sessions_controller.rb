0fe6b9e065109962b51dc5fd8d2a6a901c36cbbc
jaymuk <jaymuk.business@gmail.com>
2016-02-03 13:34:59 +0000

    flashes working as they should be, just need styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,13 +12,52 @@ class SessionsController < Devise::SessionsController
 
 
   def create
-    # self.resource = warden.authenticate!(auth_options)
-    p current_user
-    p "Does this stop here"
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    self.resource = warden.authenticate!(auth_options)
+      if resource.save
+        p "resource could save!"
+        p "============================"
+        respond_to do |format|
+          format.html {
+            yield resource if block_given?
+            if resource.active_for_authentication?
+              set_flash_message :notice, :signed_up if is_flashing_format?
+              sign_up(resource_name, resource)
+              respond_with resource, :location => after_sign_up_path_for(resource)
+              else
+              set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+              expire_data_after_sign_in!
+              respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+            end
+          }
+          format.js {
+            flash[:notice] = "Created account, signed in."
+            render :template => "remote_content/devise_success_sign_up.js.erb"
+            flash.discard
+            sign_in(resource_name, resource)
+          }
+        end
+      else
+        p "resource could not save"
+        p "============================"
+        respond_to do |format|
+        format.html {
+          clean_up_passwords resource
+          respond_with resource
+        }
+        format.js {
+          flash[:alert] = @user.errors.full_messages.to_sentence
+          render :template => "remote_content/devise_errors.js.erb"
+          flash.discard
+        }
+        end
+      end
+  end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    flash[:notice] = "Signed out."
+    yield if block_given?
+    render :template => "remote_content/remote_sign_out.js.erb"
   end
 
 
@@ -27,7 +66,7 @@ class SessionsController < Devise::SessionsController
 
   def authenticate_user!
     unless current_user
-          flash.now[:alert] = "Invalid username of password"
+          flash.now[:alert] = "Invalid username ofr password"
           render :template => "remote_content/devise_errors.js.erb"
           flash.discard(:info)
     end
