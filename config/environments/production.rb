9bf17742039dd08f8c2319efae17bdaf334f489f
jaymuk <jaymuk.business@gmail.com>
2016-02-08 23:34:26 +0000

    please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.initialize_on_precompile = true
 
