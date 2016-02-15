902391f397eb24c2c59b31ba2fc14d517bc45f51
jaymuk <jaymuk.business@gmail.com>
2016-02-15 22:21:54 +0000

    created db migration for conversation and messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122155652) do
+ActiveRecord::Schema.define(version: 20160215215403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,17 @@ ActiveRecord::Schema.define(version: 20160122155652) do
     t.datetime "updated_at", null: false
     t.integer  "listing_id"
     t.integer  "user_id"
-    t.integer  "parent_id"
   end
 
   add_index "comments", ["listing_id"], name: "index_comments_on_listing_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "favourites", force: :cascade do |t|
     t.integer  "listing_id"
@@ -48,6 +54,18 @@ ActiveRecord::Schema.define(version: 20160122155652) do
     t.string   "text"
     t.text     "prop_info"
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "pictures", force: :cascade do |t|
     t.integer  "listing_id"
